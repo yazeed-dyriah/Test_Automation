116b088ea75d245d33358c722507ca2a94dc445c
yazeed119 <79354827+yazeed-dyriah@users.noreply.github.com>
2023-05-09 07:41:06 -0700

    Open Selected Module

--- cypress/e2e/GOALs.cy.js
+++ cypress/e2e/GOALs.cy.js
@@ -13,5 +13,11 @@ describe("GOALs", function()
         cy.get('#id_username').type("yazeed").should("have.value","yazeed");
         cy.get('#id_password').type("test12345");
         cy.get('.btn.btn.btn-primary.w-100').click();
+        cy.get('#userInfoMenu').click();
+        cy.get('[href="/staff/"]').click();
+        cy.get(':nth-child(2) > :nth-child(2) > .row > .col-sm-4 > .nav > .nav-item > .nav-link').click();
     })
+    
+
+    
 });
