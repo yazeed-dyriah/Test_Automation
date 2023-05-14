40b22b8cd7016780cf626e9b32c18cd7027903a9
yazeed-dyriah <yazeedk623@gmail.com>
2023-05-14 13:03:31 +0300

    update

--- cypress/e2e/EditStaffGoalAbilities.cy.js
+++ cypress/e2e/EditStaffGoalAbilities.cy.js
@@ -1,50 +1,70 @@
-it("Open",()=>
-    {
-        cy.visit("https://goal-dev.mdx.ac.uk/accounts/login/?next=/");   
-    })
 
     it("Login",()=>
     {
-              //cy.viewport(1000, 600)
-              cy.viewport(1600, 525)
-              //Login
-              cy.visit("https://goal-dev.mdx.ac.uk/accounts/login/?next=/");
-              //username
-              cy.get("input[name='username']").type("shahd")
-              //password name='password'
-              cy.get("input[name='password']").type("shahd12345")
-              
-              
+        cy.viewport(1600, 525)
+        //Login
+        cy.visit('https://goal-dev.mdx.ac.uk/accounts/login/?next=/')
+        .then(() => {
+            cy.get('a.navbar-brand.text-light.p-0')
+            .should('exist');
+        });
 
-        //Edit staff goal abilities
+        cy.get('input[type="text"][name="username"][id="id_username"]')
+        .should('be.visible');
+        cy.get("input[name='username']").type("shahd")
+        
+        cy.get('input[type="password"][name="password"][id="id_password"]')
+        .should('be.visible');
+        cy.get("input[name='password']").type("shahd12345")
+        
+        
+        cy.get('select[name="login_as"]')
+        .should('be.visible')
+        .select('staff')
+        .should('have.value', 'staff');
         cy.get('select[name="login_as"]').select('staff');
+
+        //login button
+        cy.get('button[type="submit"].btn.btn-primary.w-100')
+        .should('exist');
         cy.get('button[type="submit"]').click();
-        cy.get('a.nav-link.active.w-100[href="/staff/25"]').click();
-        cy.get('a.nav-link.text-light[href="/staff/25/staffs/"]').click();
+
+        cy.get('a.nav-link.active.w-100') 
+        .should('be.visible');           
+        cy.get('a.nav-link.active.w-100[href="/staff/25"]').click()
+        
+        cy.contains('a.nav-link.text-light', 'Staff')
+        .should('be.visible');
+        cy.get('a.nav-link[href="/staff/25/staffs/"]').click();  
+
+
+
+        //1 Edit staff goal abilities
         cy.get('a[href="#"][onclick="showEditRow(\'AhmadMohsen\')"]').click();
+        
         cy.get('select[name="permissions_Staff"][class="textinput textInput form-control"][form="form_staff_AhmadMohsen"]').select('Read');
         cy.get('button[type="submit"][class="btn btn-primary"][form="form_staff_AhmadMohsen"]').click();
 
-        //TestCase Edit observe abilities
+        //2 TestCase Edit observe abilities
         cy.get('a[href="#"][onclick="showEditRow(\'AhmadMohsen\')"]').click();
         cy.get('select[name="permissions_Observe"][class="textinput textInput form-control"][form="form_staff_AhmadMohsen"]').select('Read');
         cy.get('button[type="submit"][class="btn btn-primary"][form="form_staff_AhmadMohsen"]').click();
-        //TestCase Edit Student abilities
+        //3 TestCase Edit Student abilities
         cy.get('a[href="#"][onclick="showEditRow(\'AhmadMohsen\')"]').click();
         cy.get('select[name="permissions_Student"][class="textinput textInput form-control"][form="form_staff_AhmadMohsen"]').select('Read');
         cy.get('button[type="submit"][class="btn btn-primary"][form="form_staff_AhmadMohsen"]').click();
-        //TestCase Edit Goals abilities
+        //4 TestCase Edit Goals abilities
         cy.get('a[href="#"][onclick="showEditRow(\'AhmadMohsen\')"]').click();
         cy.get('select[name="permissions_Goals"][class="textinput textInput form-control"][form="form_staff_AhmadMohsen"]').select('Read');
         cy.get('button[type="submit"][class="btn btn-primary"][form="form_staff_AhmadMohsen"]').click();
-        //TestCase Edit Academic abilities
+        //5 TestCase Edit Academic abilities
         cy.get('a[href="#"][onclick="showEditRow(\'AhmadMohsen\')"]').click();
         cy.get('select[name="permissions_Academic"][class="textinput textInput form-control"][form="form_staff_AhmadMohsen"]').select('Read');
         cy.get('button[type="submit"][class="btn btn-primary"][form="form_staff_AhmadMohsen"]').click();
         
 
 
-        //TestCase show all observed student
+        //6 TestCase show all observed student
         cy.get('a.nav-link.text-light[href="/staff/25/goals/"]').click();
         cy.get('input[type="checkbox"][name="all_goals"][id="all_goals"]').check({force: true});
         cy.get('input[type="checkbox"][name="goal_filter"][id="goal_1136"]').check({force: true});
@@ -56,7 +76,7 @@ it("Open",()=>
 
         //Back to the page Goal
         cy.get('a.nav-link.text-light[href="/staff/25/goals/"]').click();
-        //UnObserved all student
+        //7 UnObserved all student
         cy.get('input[type="checkbox"][name="all_goals"][id="all_goals"]').check({force: true});
         cy.get('input[type="checkbox"][name="goal_filter"][id="goal_1136"]').check({force: true});
         cy.get('input[type="checkbox"][id="topic_61"]').check({force: true});
@@ -64,46 +84,20 @@ it("Open",()=>
         cy.get('input[type="checkbox"][id="not_observed"]').uncheck({force: true});
         cy.get('button.btn.btn-primary.mt-2.w-100').contains('Apply filters').click();
         cy.get('button.btn.btn-danger.mt-2.w-100').contains('Unobserve').click();
-        //Observed all student
+        
+        //8 Observed all student in all groups with all levels
         cy.get('button.btn.btn-primary.mt-2.w-100').contains('Observe').click();
         
         //cy.get('button.btn.btn-danger.mt-2.w-100').contains('Unobserve').click();
 
         //cy.get('button.btn btn-primary[type="button"][class="btn btn-primary"]').click();
 
-        //TestCase give all student same grade
+        //9   give all student same grade
         cy.get('select[name="grade"][class="form-control mt-2 w-100 pr-0"][ onchange="gradeAllSelected(this)"]').select('3', { force: true });
 
-        //TestCase except groupB
-
-
+        //10   Observe all groups except groupB
         cy.get('input[type="checkbox"][class="custom-control-input topic_filter"][id="topic_60"]').check({force: true});
-    
-    
-                                   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        cy.get('button.btn.btn-primary.mt-2.w-100').contains('Observe').click();
 
 
         /*
